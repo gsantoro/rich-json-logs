5068f70ee2b4fd86d4a4ae9d1f6b66ad639c6643
gsantoro <giuseppe.santoro@elastic.co>
2022-11-28 12:38:42 +0000

    live implementation + mypy

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 
 import sys
-import click
 from rich_json_logs.log import ColoredLogs
+import click
 from rich_json_logs.highlighter import LogHighlighter, LogTheme
-
 
 @click.command()
 @click.option("-i", "--input-path", help="Input path")
 @click.option("-c", "--columns", default="@timestamp, log.level, message", help="Columns to filter")
-def main(input_path, columns):
+@click.option("--live/--no-live", default=False, help="Live streaming or static with pager")
+@click.option("--sleep", default=0.4, help="Time to sleep between adding rows to live streaming")
+def main(input_path, columns, live, sleep):    
     highlighter = LogHighlighter()
     theme = LogTheme
-
+    
     if input_path is None:
         input_file = sys.stdin
     else:
         input_file = open(input_path, 'r')
 
     log = ColoredLogs("Logs", columns, input_file, highlighter, theme)
-    log.process()
-
+    
+    if live:
+        log.live_table(sleep)
+    else:
+        log.table_with_pager()
 
 if __name__ == '__main__':
     main()
--- rich_json_logs/log.py
+++ rich_json_logs/log.py
@@ -3,10 +3,11 @@ import time
 
 from rich.console import Console
 from rich.table import Table, Column
+from rich.live import Live
 from rich.markup import escape
 
 from jsonpath_ng import parse
-
+from rich.prompt import Prompt
 
 class ColoredLogs:
     def __init__(self, title, columns, input, highlighter, theme):
@@ -27,7 +28,7 @@ class ColoredLogs:
                        overflow="fold")
             ans.append(c)
         return ans
-
+    
     def parse_jsonpath(self, header, d):
         try:
             jsonpath_expression = parse(header)
@@ -36,15 +37,13 @@ class ColoredLogs:
             return v
         except Exception as e:
             return "-"
-
-    def process(self):
-        table = Table(*self.columns, title=self.title, highlight=True)
-
+        
+    def rows(self):
         line = self.input.readline()
         while line:
             try:
                 d = json.loads(line)
-
+                
                 values = []
                 for col in self.columns:
                     if str(col.header).startswith("$"):
@@ -53,19 +52,49 @@ class ColoredLogs:
                         v = "-"
                     else:
                         v = escape(d[col.header])
-
                     values.append(v)
-
-                table.add_row(*values)
+                yield values
             except Exception as e:
                 row = len(self.columns) * ["-"]
                 row[-1] = line.strip()
-                table.add_row(*row)
-
+                yield row
             line = self.input.readline()
+        yield None
 
+    def build_console(self):
         console = Console(highlighter=self.highlighter,
                           theme=self.theme,
                           force_terminal=True)
+        return console
+    
+    def build_table(self):
+        table = Table(*self.columns,
+                      title=self.title,
+                      highlight=True)
+        return table
+
+    def table_with_pager(self):
+        table = self.build_table()
+        console = self.build_console()
+        
+        for row in self.rows():
+            if not row:
+                break
+            table.add_row(*row)
+        
         with console.pager(styles=True):
             console.print(table)
+
+    def live_table(self, sleep):        
+        table = self.build_table()
+        console = self.build_console()
+        
+        with Live(table, auto_refresh=False,
+                  console=console,
+                  vertical_overflow="visible") as l:    
+            for row in self.rows():
+                if not row:
+                    break
+                table.add_row(*row)
+                time.sleep(sleep)
+                l.refresh()
